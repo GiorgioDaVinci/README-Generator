6ccad43a81df109f60f022739126214027c87d6d
GiorgioDaVinci <okoriegeorge1@gmail.com>
2024-02-18 12:18:04 +0000

    Minor code corrections

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -3,19 +3,19 @@ function renderLicense(license){
   if(license === 'MIT'){
     return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
   }
-  else if(license === 'Eclipse'){
+  if(license === 'Eclipse'){
     return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
   }
-  else if(license === 'Apache'){
+  if(license === 'Apache'){
     return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
   }
-  else if(license === 'IBM'){
+  if(license === 'IBM'){
     return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
   }
-  else if(license === 'Mozilla'){
+  if(license === 'Mozilla'){
     return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
   }
-  else if(license === 'ISC'){
+  if(license === 'ISC'){
     return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
   }
 }
